6566ec1064cc668fa02d7b5bdf3b5c7f406b90e3
jwlehman <jeremylehman9@gmail.com>
2016-01-01 23:36:40 -0500

    added input.txt

---
+++ scala/247/SecretSanta.scala
@@ -0,0 +1,12 @@
+/**
+  * Created by jwlehman on 1/1/16.
+  *
+  * challenge url: https://www.reddit.com/r/dailyprogrammer/comments/3yiy2d/20151228_challenge_247_easy_secret_santa/
+  */
+import scala.io.Source
+
+class SecretSanta {
+
+
+
+}
